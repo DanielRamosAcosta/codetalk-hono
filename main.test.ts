8c30ee53aa01331f2019347264f5819b150ec57f
Daniel Ramos <danielramosacosta1@gmail.com>
2024-08-11 16:19:14 +0200

    minimal repro

--- main.test.ts
+++ main.test.ts
@@ -3,47 +3,16 @@ import { expect } from "expect";
 import { app as honoApp } from "./app.ts";
 import { CONCHA_ASENSIO } from "./people.ts";
 
-class TestClient {
-  app: typeof honoApp;
-
-  constructor(app: typeof honoApp) {
-    this.app = app;
-  }
-
-  async registerSpeaker({ id = CONCHA_ASENSIO.id, expectedStatus = 201 } = {}) {
-    const res = await this.app.request("/api/v1/speakers/registration", {
-      method: "POST",
-      body: JSON.stringify({
-        id,
-        email: CONCHA_ASENSIO.email,
-        password: CONCHA_ASENSIO.password,
-      }),
-    });
-    expect(res.status).toBe(expectedStatus);
-    return res;
-  }
-
-  async loginSpeaker({ expectedStatus = 200 } = {}) {
-    const res = await this.app.request("/api/v1/speakers/login", {
-      method: "POST",
-      body: JSON.stringify({
-        email: CONCHA_ASENSIO.email,
-        password: CONCHA_ASENSIO.password,
-      }),
-    });
-    expect(res.status).toBe(expectedStatus);
-    return res;
-  }
-}
-
 describe("main", () => {
-  it("should work", async () => {
-    const testClient = new TestClient(honoApp);
-
-    await testClient.registerSpeaker();
-    // const res = await testClient.loginSpeaker();
-    // const body = await res.json();
-
-    // expect(body.accessToken).toBe(CONCHA_ASENSIO.jwt);
+  it("should fail if not all params are passed", async () => {
+    const res = await honoApp.request("/api/v1/speakers/registration", {
+      method: "POST",
+      body: JSON.stringify({
+        id: CONCHA_ASENSIO.id,
+        // email: CONCHA_ASENSIO.email,
+        password: CONCHA_ASENSIO.password,
+      }),
+    });
+    expect(res.status).toBe(400);
   });
 });
